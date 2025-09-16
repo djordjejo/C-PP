3e5373a98b5a155331b8f26a76283acfd2dd19d7
Djordje Jovanovic <dj.jovanovic6@gmail.com>
2025-09-16 18:06:06 +0200

    Created databse

--- C#_PP/Program.cs
+++ C#_PP/Program.cs
@@ -1,4 +1,9 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ProjectDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Data/ProjectDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Domain_names;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Products> Products { get; set; }   
+        public DbSet<Categories> Categories { get; set; }   
+        public DbSet<ProductCategories> ProductCategories{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductCategories>()
+                .HasKey(pc => new { pc.ProductId, pc.CategoryId });
+
+            modelBuilder.Entity<ProductCategories>()
+                .HasOne(pc => pc.Product)
+                .WithMany()
+                .HasForeignKey(pc => pc.ProductId);
+
+            modelBuilder.Entity<ProductCategories>()
+                .HasOne(pc => pc.Category)
+                .WithMany()
+                .HasForeignKey(pc => pc.CategoryId);
+        }
+    }
+}
