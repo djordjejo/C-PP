1233cb599beb3b04b48d8604b8389b51521514ea
Djordje Jovanovic <dj.jovanovic6@gmail.com>
2025-09-16 17:23:45 +0200

    Added data_models

---
+++ Models/Domain names/ProductCategories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Domain_names
+{
+    public class ProductCategories
+    {
+        public Guid ProductId { get; set; }
+        public Guid CategoryId { get; set; }
+
+
+        public Products Product { get; set; }
+        public Categories Category { get; set; }
+
+    }
+}
---
+++ Models/Domain names/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Domain_names
+{
+    public class Products
+    {
+        [Key]
+        public Guid ProductId { get; set; }
+        [Required (ErrorMessage = "Product name is required field")]
+        public string ProductName{ get; set; }
+        [Required(ErrorMessage = "Product price is required field")]
+
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Product description is required field")]
+
+        public string Description{ get; set; }
+        [Required]
+        public DateTime StartAt { get; set; }
+    }
+}
